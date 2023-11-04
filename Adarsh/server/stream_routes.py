ff50d0a3fbe6d8dcc2e7f73d5fe162e047a43683
joaq13 <100519450+joaq13@users.noreply.github.com>
2023-11-04 19:15:04 +0100

    stream_routes.py

--- Adarsh/server/stream_routes.py
+++ Adarsh/server/stream_routes.py
@@ -26,7 +26,7 @@ async def root_route_handler(_):
         {
             "server_status": "running",
             "uptime": get_readable_time(time.time() - StartTime),
-            "telegram_bot": "@" + StreamBot.username,
+            
             "connected_bots": len(multi_clients),
             "loads": dict(
                 ("bot" + str(c + 1), l)
